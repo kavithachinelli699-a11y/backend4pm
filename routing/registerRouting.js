423fd1afcd5fbcae50d85b0cf6a4701e051287b5
Kavitha chinnalli <kavithachinelli699@gmail.com>
2026-07-21 10:51:14 +0530

    Remove unused Slide import from registerRouting.js

--- routing/registerRouting.js
+++ routing/registerRouting.js
@@ -1,7 +1,6 @@
 const express=require('express');
 const nodemailer = require("nodemailer");
 const Register = require('../model/Registermodel');
-const { Slide } = require('react-toastify/unstyled');
 const registerRouter = express.Router();
 
 
